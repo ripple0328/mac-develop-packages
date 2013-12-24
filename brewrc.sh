99373401d0a80e6e37ddd518ebb621823e4ad845
Qingbo <ripple0328@gmail.com>
2013-12-24 12:48:16 +0800

    add pow and anvil

--- brewrc.sh
+++ brewrc.sh
@@ -46,4 +46,8 @@ check-and-cask-install google-chrome
 check-and-cask-install firefox
 check-and-cask-install textmate
 check-and-cask-install rubymine
+check-and-cask-install anvil
+
+install_pow
+
 
